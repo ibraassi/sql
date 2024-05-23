881e6da817b0eef87186ed0698f2ee1b221ef5ff
ibraassi <166841360+ibraassi@users.noreply.github.com>
2024-05-22 23:26:03 -0400

    Update homework_4.sql

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -16,8 +16,10 @@ HINT: keep the syntax the same, but edited the correct components with the strin
 The `||` values concatenate the columns into strings. 
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
-
-
+SELECT 
+product_name || ', ' || coalesce(product_size,'')|| ' (' || coalesce(product_qty_type,'unit') || ')'
+AS product_list
+FROM product;
 
 
 --Windowed Functions
@@ -29,12 +31,77 @@ You can either display all rows in the customer_purchases table, with the counte
 each new market date for each customer, or select only the unique market dates per customer 
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
+SELECT DISTINCT customer_id, market_date
+,dense_rank()OVER(PARTITION BY customer_id ORDER BY market_date ASC) AS visit_number
+FROM customer_purchases;
 
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
+SELECT customer_id, market_date
+FROM(
+SELECT DISTINCT customer_id, market_date
+,dense_rank()OVER(PARTITION BY customer_id ORDER BY market_date DESC) AS visit_number
+FROM customer_purchases
+) AS x
+WHERE x.visit_number = 1;
 
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+SELECT *
+,count()OVER(PARTITION BY product_id,customer_id ORDER BY market_date,transaction_time ASC)
+AS rolling_count
+FROM customer_purchases;
+
+
+-- String manipulations
+/* 1. Some product names in the product table have descriptions like "Jar" or "Organic".
+These are separated from the product name with a hyphen.
+Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL.
+Remove any trailing or leading whitespaces. Don't just use a case statement for each product!
+
+| product_name               | description |
+|----------------------------|-------------|
+| Habanero Peppers - Organic | Organic     |
+
+HINT: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+SELECT product_name
+,rtrim(ltrim(substr(product_name,nullif(instr(product_name,'-')+1,1)))) AS description
+FROM product;
+
+
+/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+SELECT * FROM product
+WHERE product_size REGEXP '[0-9]';
+
+
+-- UNION
+/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
+
+HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following:
+1) Create a CTE/Temp Table to find sales values grouped dates;
+2) Create another CTE/Temp table with a rank windowed function on the previous query to create
+"best day" and "worst day";
+3) Query the second temp table twice, once for the best day, once for the worst day,
+with a UNION binding them. */
+DROP TABLE IF EXISTS sales_table;
+CREATE TEMP TABLE sales_table AS
+SELECT market_date
+,sum(quantity*cost_to_customer_per_qty) AS sales
+FROM customer_purchases
+GROUP BY market_date;
+DROP TABLE IF EXISTS ranked_table;
+CREATE TEMP TABLE ranked_table AS
+SELECT *
+,rank()OVER(ORDER BY sales DESC) AS best
+,rank()OVER(ORDER BY sales ASC) AS worst
+FROM sales_table;
+SELECT market_date,sales
+FROM ranked_table
+WHERE best = 1
+UNION
+SELECT market_date,sales
+FROM ranked_table
+WHERE worst = 1;
