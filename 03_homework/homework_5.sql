a3568230d6af1d4053552b1d79cd7a755e2f2b7b
ibraassi <166841360+ibraassi@users.noreply.github.com>
2024-05-24 14:00:21 -0400

    Update homework_5.sql

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -1,33 +1,3 @@
--- String manipulations
-/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
-These are separated from the product name with a hyphen. 
-Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
-Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
-
-| product_name               | description |
-|----------------------------|-------------|
-| Habanero Peppers - Organic | Organic     |
-
-Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
-
-
-
-/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
-
--- UNION
-/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
-
-HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
-2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
-"best day" and "worst day"; 
-3) Query the second temp table twice, once for the best day, once for the worst day, 
-with a UNION binding them. */
-
-
-
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
 customer on record. How much money would each vendor make per product? 
@@ -38,7 +8,18 @@ Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely b
 Think a bit about the row counts: how many distinct vendors, product names are there (x)?
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
-
+SELECT vendor_name, product_name
+,sum(5*original_price) AS sales
+FROM(
+SELECT DISTINCT v.vendor_name, p.product_name, vi.original_price, c.customer_id
+FROM vendor_inventory AS vi
+INNER JOIN vendor AS v
+ON vi.vendor_id = v.vendor_id
+INNER JOIN product AS p
+ON vi.product_id = p.product_id
+CROSS JOIN customer AS c
+) AS x
+GROUP BY vendor_name,product_name;
 
 
 -- INSERT
@@ -46,19 +27,30 @@ Before your final group by you should have the product of those two queries (x*y
 This table will contain only products where the `product_qty_type = 'unit'`. 
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
-
+DROP TABLE IF EXISTS product_units;
+CREATE TABLE product_units AS
+SELECT *, CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
-
+INSERT INTO product_units
+VALUES(7,'Apple Pie','10"',3,'unit',CURRENT_TIMESTAMP);
 
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
-
+DELETE FROM product_units
+WHERE(product_id,snapshot_timestamp) IN(
+SELECT pu1.product_id, pu1.snapshot_timestamp
+FROM product_units AS pu1
+INNER JOIN product_units AS pu2
+ON pu1.product_id = pu2.product_id
+WHERE pu1.snapshot_timestamp < pu2.snapshot_timestamp);
 
 
 -- UPDATE
@@ -77,5 +69,21 @@ Third, SET current_quantity = (...your select statement...), remembering that WH
 Finally, make sure you have a WHERE statement to update the right row, 
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
-
-
+DROP TABLE IF EXISTS product_quantity;
+CREATE TEMP TABLE product_quantity AS
+SELECT product_id
+,coalesce(quantity, 0) AS last_quantity
+FROM(
+SELECT p.product_id, vi.quantity
+,rank()OVER(PARTITION BY vi.product_id ORDER BY vi.market_date DESC) AS last
+FROM product AS p
+LEFT JOIN vendor_inventory AS vi
+ON p.product_id = vi.product_id
+) as x
+WHERE last = 1
+ORDER BY product_id;
+ALTER TABLE product_units
+ADD current_quantity INT;
+UPDATE product_units
+SET current_quantity = (SELECT last_quantity FROM product_quantity
+WHERE product_units.product_id = product_quantity.product_id);
